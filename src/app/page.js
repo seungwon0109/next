dc1397aec42329f6b190fe23dec0af1085f1b92d
seungwon0109 <vbhvbh0109@naver.com>
2024-06-19 01:06:36 +0900

    1

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,27 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return <>next js</>;
+  return (
+    <html>
+      <body>
+        <div class="navbar">
+          <div class="logo">Next</div>
+          <div class="navbar-links">
+            <a href="#">홈</a>
+            <a href="#">서비스</a>
+            <a href="#">소개</a>
+            <a href="#">연락처</a>
+          </div>
+        </div>
+
+        <div class="introduction">
+          <h1>Nextjs란</h1>
+          <p>
+            Next.js는 풀스택 웹 어플리케이션을 구축하기 위한 React 프레임워크
+            이다
+          </p>
+        </div>
+      </body>
+    </html>
+  );
 }
